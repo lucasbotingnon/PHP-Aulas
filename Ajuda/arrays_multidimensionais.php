807d6b3c2538f0b36c9b69a3485c47b377131a31
Lucas <lucas.botingnon@gmail.com>
2018-02-07 19:26:28 -0400

    Arrays multidimensionais

---
+++ Ajuda/arrays_multidimensionais.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="estilo.css">
+		<title>Mensagens divertidas</title>
+	</head>
+
+	<body>
+
+		<?php 
+
+			$tabuleiro[8]['a'] = 'torre preta';
+			$tabuleiro[8]['b'] = 'cavalo preto';
+			$tabuleiro[8]['c'] = 'bispo preto';
+			$tabuleiro[8]['d'] = 'rei preto';
+			$tabuleiro[8]['e'] = 'rainha preta';
+			$tabuleiro[8]['f'] = 'bispo preto'
+			$tabuleiro[8]['g'] = 'cavalo preto';
+			$tabuleiro[8]['h'] = 'torre preta';
+
+			$tabuleiro[7]
+			$tabuleiro[6]
+			$tabuleiro[5]
+			$tabuleiro[4]
+			$tabuleiro[3]
+			$tabuleiro[2]
+			$tabuleiro[1]
+
+		 ?>
+
+	</body>
+</html>
