d1e89aed8061d1ba2e71a2e41891d96506537065
Lucas <lucas.botingnon@gmail.com>
2018-02-07 20:55:05 -0400

    if/else pt2

---
+++ Ajuda/if_else.php
@@ -0,0 +1,23 @@
+<?php 
+
+// ==
+// ===
+// != <>	diferente
+// !===
+// <
+// >
+// <=
+// >=
+
+// Operadores Aritmeticos
+// && ou AND -> retorna verdadeiro se todos os resultados das expressoes forem verdade
+// || ou OR  -> retorna verdadeiro se uma das expressoes forem verdadeiras
+// XOR 		 -> retorna verdadeiro apenas se 1 das expressoes for verdadeira
+// ! 		 -> inverter o resultado
+if (true) {
+	echo "Verdadeiro";
+}else{
+	echo "Falso";
+}
+
+?>
